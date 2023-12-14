5a6779f59eb508440749597bd0ae547e99f3f163
Siqlain <144030556+Siqlain@users.noreply.github.com>
2023-12-14 12:30:01 +0500

    Fixed interactivity issue with IDWise SDK

--- client/scripts/controllers/enter.controller.js
+++ client/scripts/controllers/enter.controller.js
@@ -5,129 +5,131 @@
  */
 
 Meteor.startup(function () {
+  angular.module("Ayamobile").controller("EnterCtrl", EnterCtrl);
+  function EnterCtrl($scope, $reactive) {
+    // var script = document.createElement("script");
+    // script.onload = function (e) {
+    //   console.log("script loaded", e);
+    // };
+    // script.src = "https://fix-cordova-dom-exception.idwise.com/idwise.min.js";
+    // document.head.appendChild(script);
 
-    angular
-        .module('Ayamobile')
-        .controller('EnterCtrl', EnterCtrl);
+    $reactive(this).attach($scope);
+    $scope.$on("$ionicView.afterEnter", function () {});
 
-
-    function EnterCtrl( $scope, $reactive) {
-        $reactive(this).attach($scope);
-        $scope.$on('$ionicView.afterEnter', function () {
-          
-           
-            });
-
-
-            this.testCode = testCode;
-        function testCode(){
-            console.log('testCode');
-            if (Meteor.isCordova) {
-                document.addEventListener("deviceready", onDeviceReady, false);
-              }
-            }
-        
-        
-            function onDeviceReady() {
-                console.log("Device is ready");
-                checkCameraPermissionState();
-              }
-              function handleError(error) {
-                console.log("An error occurred: " + error);
-              }
-              function checkCameraPermissionState() {
-                if (Meteor.isCordova) {
-                  cordova.plugins.diagnostic.getCameraAuthorizationStatus(function (
-                    status
-                  ) {
-                  ///  setPermissionStatus(status);
-                  $scope.permissionStatus = status;
-                  console.log('status' + status);
-                  if(status != "GRANTED"){
-                    requestCameraPermissions();
-                  }
-                    if (status === "GRANTED") {
-                      console.log("Permission granted loading IDWise");
-                      loadIDWise();
-                    }
-                  },
-                  handleError);
-                }
-              }
-            
-              function requestCameraPermissions() {
-                if (Meteor.isCordova) {
-                  cordova.plugins.diagnostic.requestCameraAuthorization({
-                    successCallback: function (status) {
-                      switch (status) {
-                        case "NOT_REQUESTED":
-                          console.log("Permission not requested");
-                          break;
-                        case "DENIED_ALWAYS":
-                          console.log("Permission denied");
-                          break;
-                        case "GRANTED":
-                        case "LIMITED": // iOS 14+
-                        //   console.log("Permission granted", status);
-                         loadIDWise();
-                        //   setPermissionStatus("GRANTED");
-                          break;
-                      }
-                      console.log(
-                        "Successfully requested camera authorization: authorization was " +
-                          status
-                      );
-                    },
-                    errorCallback: handleError,
-                    externalStorage: true,
-                  });
-                }
-              }
-            
-              function loadIDWise() {
-                if ($scope.permissionStatus !== "GRANTED") {
-                    requestCameraPermissions();
-                }
-                try {
-                //  setIsLoading(true);
-                  console.log('try to start');
-                  IDWise.initialize({
-                    clientKey: "QmFzaWMgTW1ZM05XTTJZMkl0Tm1VMVpDMDBZV1ZqTFdKa04yUXRZemcwTkdOaVpHRmhOVGN4T2sweVlsQlVNVmh4TVV4bmJFTkdXVVV6TmpseVUxRkdTSE53YlU5cGREQTFaWHBCVm1STFJHOD0=",
-                    locale: "ar",
-                  }).then(idwise => {
-                console.log('idwise' + JSON.stringify(idwise));
-                    // var template = '<div class="row row-centered">' +
-                    // '    <div id="idwise-temp"></div>' +
-                    // '</div>';
-                    idwise.startJourney({
-                      mount: "#idwise-mount",
-                      journeyDefinitionId: "2f75c6cb-6e5d-4aec-bd7d-c844cbdaa571",
-                      referenceNo: "test",
-                      eventHandlers: {
-                        onJourneyStarted: function (details) {
-                          alert("Journey started, journey id =" + details.journeyId);
-                        },
-                        onJourneyFinished: function (details) {
-                          alert("Journey finished, journey id =" + details.journeyId);
-                        },
-                        onJourneyCancelled: function (details) {
-                          alert("Journey cancelled, journey id =" + details.journeyId);
-                        },
-                      },
-                    });
-             
-                  }).catch(error => {
-                    alert(error);
-                  //  setIsLoading(false);
-                  });
-                } catch (error) {
-                  alert(error);
-                 // setIsLoading(false);
-                }
-            
-            
-            
-            }
+    this.testCode = testCode;
+    function testCode() {
+      console.log("testCode");
+      // To test on web uncomment the following lines
+      // $scope.permissionStatus = "GRANTED";
+      // loadIDWise();
+      if (Meteor.isCordova) {
+        document.addEventListener("deviceready", onDeviceReady, false);
+      }
     }
-       
+
+    function onDeviceReady() {
+      console.log("Device is ready");
+      checkCameraPermissionState();
+    }
+    function handleError(error) {
+      console.log("An error occurred:  " + error);
+    }
+    function checkCameraPermissionState() {
+      if (Meteor.isCordova) {
+        cordova.plugins.diagnostic.getCameraAuthorizationStatus(function (
+          status
+        ) {
+          ///  setPermissionStatus(status);
+
+          $scope.permissionStatus = status;
+          console.log("status" + status);
+          if (status != "GRANTED") {
+            requestCameraPermissions();
+          }
+          if (status === "GRANTED") {
+            console.log("Permission granted loading IDWise");
+            loadIDWise();
+          }
+        },
+        handleError);
+      }
+    }
+
+    function requestCameraPermissions() {
+      if (Meteor.isCordova) {
+        cordova.plugins.diagnostic.requestCameraAuthorization({
+          successCallback: function (status) {
+            switch (status) {
+              case "NOT_REQUESTED":
+                console.log("Permission not requested");
+                break;
+              case "DENIED_ALWAYS":
+                console.log("Permission denied");
+                break;
+              case "GRANTED":
+              case "LIMITED": // iOS 14+
+                //   console.log("Permission granted", status);
+                $scope.permissionStatus = "GRANTED";
+                loadIDWise();
+                //   setPermissionStatus("GRANTED");
+                break;
+            }
+            console.log(
+              "Successfully requested camera authorization: authorization was " +
+                status
+            );
+          },
+          errorCallback: handleError,
+          externalStorage: true,
+        });
+      }
+    }
+
+    function loadIDWise() {
+      if ($scope.permissionStatus !== "GRANTED") {
+        requestCameraPermissions();
+        //Return if permission is not granted else if will recursively call loadIDWise
+        return;
+      }
+      try {
+        //  setIsLoading(true);
+        console.log("try to start");
+        IDWise.initialize({
+          clientKey:
+            "QmFzaWMgTW1ZM05XTTJZMkl0Tm1VMVpDMDBZV1ZqTFdKa04yUXRZemcwTkdOaVpHRmhOVGN4T2sweVlsQlVNVmh4TVV4bmJFTkdXVVV6TmpseVUxRkdTSE53YlU5cGREQTFaWHBCVm1STFJHOD0=",
+          locale: "en",
+        })
+          .then(idwise => {
+            console.log("idwise", idwise);
+            // var template = '<div class="row row-centered">' +
+            // '    <div id="idwise-temp"></div>' +
+            // '</div>';
+            idwise.startJourney({
+              mount: "#idwise-mount",
+              journeyDefinitionId: "2f75c6cb-6e5d-4aec-bd7d-c844cbdaa571",
+              referenceNo: "test",
+              eventHandlers: {
+                onJourneyStarted: function (details) {
+                  alert("Journey started, journey id =" + details.journeyId);
+                },
+                onJourneyFinished: function (details) {
+                  alert("Journey finished, journey id =" + details.journeyId);
+                },
+                onJourneyCancelled: function (details) {
+                  alert("Journey cancelled, journey id =" + details.journeyId);
+                },
+              },
+            });
+          })
+          .catch(error => {
+            alert(error);
+            //  setIsLoading(false);
+          });
+      } catch (error) {
+        alert(error);
+        // setIsLoading(false);
+      }
+    }
+  }
 });
--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -3,43 +3,43 @@
  * Company: Aya Jo
  * Created by Prince Hernandez, Yazeed Samamah.
  */
-angular
-    .module('Ayamobile')
-    .config(config);
+angular.module("Ayamobile").config(config);
 
-function config($stateProvider, $urlRouterProvider, $ionicConfigProvider, $translateProvider, $locationProvider) {
-    $ionicConfigProvider.navBar.alignTitle('center');
-    $ionicConfigProvider.views.transition('platform');
-    $ionicConfigProvider.spinner.icon('lines');
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
+function config(
+  $stateProvider,
+  $urlRouterProvider,
+  $ionicConfigProvider,
+  $translateProvider,
+  $locationProvider
+) {
+  $ionicConfigProvider.navBar.alignTitle("center");
+  $ionicConfigProvider.views.transition("platform");
+  $ionicConfigProvider.spinner.icon("lines");
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false,
+  });
 
-    /*   uiGmapGoogleMapApiProvider.configure({
+  /*   uiGmapGoogleMapApiProvider.configure({
            key: 'AIzaSyAbl8ERZhvHgnh6CXFVe3s_U3D6YHGx0Ao',
            libraries: 'weather,geometry,visualization'
        });*/
 
-    $stateProvider
-        .state('enter', {
-            url: '/enter',
-            templateUrl: 'client/templates/enter.html',
-            controller: 'EnterCtrl as enter',
-            fastRender: true
-        })
-        
-   
-        ;
+  $stateProvider.state("enter", {
+    url: "/enter",
+    templateUrl: "client/templates/enter.html",
+    controller: "EnterCtrl as enter",
+    fastRender: true,
+  });
 
-    $urlRouterProvider.otherwise('enter');
-    //Lets Config the translations
+  $urlRouterProvider.otherwise("enter");
+  //Lets Config the translations
 
-    $translateProvider.useSanitizeValueStrategy('escapeParameters');
-    $translateProvider.useStaticFilesLoader({
-        prefix: 'Languages/',
-        suffix: '.json'
-    });
-    $translateProvider.preferredLanguage('ar');
-    $translateProvider.use('en');
+  $translateProvider.useSanitizeValueStrategy("escapeParameters");
+  $translateProvider.useStaticFilesLoader({
+    prefix: "Languages/",
+    suffix: ".json",
+  });
+  $translateProvider.preferredLanguage("ar");
+  $translateProvider.use("en");
 }
