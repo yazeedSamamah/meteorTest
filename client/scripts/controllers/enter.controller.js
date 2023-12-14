67eaf5dcc6dcd5a2ee9ae059a81c924677bc3dbf
IDWise <idwise@IDWises-MacBook-Pro.local>
2023-12-14 18:24:02 +0500

    Added platform io

--- client/scripts/controllers/enter.controller.js
+++ client/scripts/controllers/enter.controller.js
@@ -43,11 +43,11 @@ Meteor.startup(function () {
           ///  setPermissionStatus(status);
 
           $scope.permissionStatus = status;
-          console.log("status" + status);
-          if (status != "GRANTED") {
+          console.log("status", status);
+          if (status != "GRANTED" || status != "authorized") {
             requestCameraPermissions();
           }
-          if (status === "GRANTED") {
+          if (status === "GRANTED" || status === "authorized") {
             console.log("Permission granted loading IDWise");
             loadIDWise();
           }
@@ -67,6 +67,7 @@ Meteor.startup(function () {
               case "DENIED_ALWAYS":
                 console.log("Permission denied");
                 break;
+              case "authorzed":
               case "GRANTED":
               case "LIMITED": // iOS 14+
                 //   console.log("Permission granted", status);
@@ -87,48 +88,55 @@ Meteor.startup(function () {
     }
 
     function loadIDWise() {
-      if ($scope.permissionStatus !== "GRANTED") {
+      if (
+        $scope.permissionStatus === "GRANTED" ||
+        $scope.permissionStatus === "authorized"
+      ) {
+        try {
+          //  setIsLoading(true);
+          console.log("try to start");
+          IDWise.initialize({
+            clientKey:
+              "QmFzaWMgTW1ZM05XTTJZMkl0Tm1VMVpDMDBZV1ZqTFdKa04yUXRZemcwTkdOaVpHRmhOVGN4T2sweVlsQlVNVmh4TVV4bmJFTkdXVVV6TmpseVUxRkdTSE53YlU5cGREQTFaWHBCVm1STFJHOD0=",
+            locale: "en",
+          })
+            .then(idwise => {
+              console.log("idwise", idwise);
+              // var template = '<div class="row row-centered">' +
+              // '    <div id="idwise-temp"></div>' +
+              // '</div>';
+              idwise.startJourney({
+                mount: "#idwise-mount",
+                journeyDefinitionId: "2f75c6cb-6e5d-4aec-bd7d-c844cbdaa571",
+                referenceNo: "test",
+                eventHandlers: {
+                  onJourneyStarted: function (details) {
+                    alert("Journey started, journey id =" + details.journeyId);
+                  },
+                  onJourneyFinished: function (details) {
+                    alert("Journey finished, journey id =" + details.journeyId);
+                  },
+                  onJourneyCancelled: function (details) {
+                    alert(
+                      "Journey cancelled, journey id =" + details.journeyId
+                    );
+                  },
+                },
+              });
+            })
+            .catch(error => {
+              alert(error);
+              //  setIsLoading(false);
+            });
+        } catch (error) {
+          alert(error);
+          // setIsLoading(false);
+        }
+      } else {
+        console.log("Permission not granted", $scope.permissionStatus);
         requestCameraPermissions();
         //Return if permission is not granted else if will recursively call loadIDWise
         return;
-      }
-      try {
-        //  setIsLoading(true);
-        console.log("try to start");
-        IDWise.initialize({
-          clientKey:
-            "QmFzaWMgTW1ZM05XTTJZMkl0Tm1VMVpDMDBZV1ZqTFdKa04yUXRZemcwTkdOaVpHRmhOVGN4T2sweVlsQlVNVmh4TVV4bmJFTkdXVVV6TmpseVUxRkdTSE53YlU5cGREQTFaWHBCVm1STFJHOD0=",
-          locale: "en",
-        })
-          .then(idwise => {
-            console.log("idwise", idwise);
-            // var template = '<div class="row row-centered">' +
-            // '    <div id="idwise-temp"></div>' +
-            // '</div>';
-            idwise.startJourney({
-              mount: "#idwise-mount",
-              journeyDefinitionId: "2f75c6cb-6e5d-4aec-bd7d-c844cbdaa571",
-              referenceNo: "test",
-              eventHandlers: {
-                onJourneyStarted: function (details) {
-                  alert("Journey started, journey id =" + details.journeyId);
-                },
-                onJourneyFinished: function (details) {
-                  alert("Journey finished, journey id =" + details.journeyId);
-                },
-                onJourneyCancelled: function (details) {
-                  alert("Journey cancelled, journey id =" + details.journeyId);
-                },
-              },
-            });
-          })
-          .catch(error => {
-            alert(error);
-            //  setIsLoading(false);
-          });
-      } catch (error) {
-        alert(error);
-        // setIsLoading(false);
       }
     }
   }
